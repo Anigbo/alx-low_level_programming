bd19b87d2452dd1958a134aa82a56cbdc5118a03
Fredrick Anigbo <anigbofredrick@gmail.com>
2022-09-09 13:03:57 -0700

    updated

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,17 +10,19 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i < 90; i++)
 	{
-		putchar((i / 10) + '0');
-		putchar((i % 10) + '0');
-		if (i != 99)
+		for ()
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+		}
+		if (i != 89)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
