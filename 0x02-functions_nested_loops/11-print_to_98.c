a349c1dcbec79cc6793302c0e773ecbdd5a3a61a
Fredrick Anigbo <anigbofredrick@gmail.com>
2022-09-15 14:01:54 -0700

    print all narural number from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,13 +9,12 @@
  */
 void print_to_98(int n)
 {
-	if (n < 98)
+	if (n <= 98)
 	{
 		for (n = 0; n <= 97; n++)
 			printf("%d, ", n);
 	}
 	else
-	
 		for (n = 0; n > 98; n--)
 			printf("%d, ", n);
 	printf("98\n");
