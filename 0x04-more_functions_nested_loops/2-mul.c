4af08b8817c31020b571f047653533e7d89c045d
Fredrick Anigbo <anigbofredrick@gmail.com>
2022-09-17 13:57:09 -0700

    A program that multiplies 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 numbers
+ * @a: first number to be multipled
+ * @b: second number to be multipled with first
+ *
+ * Return: The result of multiplication a param and b param
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
