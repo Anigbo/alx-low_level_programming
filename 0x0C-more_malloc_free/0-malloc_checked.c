72254065aa9a84c5f782ee9582ba1192f5cb6a27
Fredrick Anigbo <anigbofredrick@gmail.com>
2022-10-07 23:38:05 -0700

    allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,7 +6,7 @@
  * malloc_checked - allocates memory
  * @b: the size to allocate
  *
- * Return: Nothing
+ * Return: pointer to the allocated memory or normal process termination of error
  */
 void *malloc_checked(unsigned int b)
 {
