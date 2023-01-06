1f40269af8057fcc4cbedb371a484f8bf8e28218
Fredrick Anigbo <anigbofredrick@gmail.com>
2023-01-06 11:57:28 -0800

    updated

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,35 +1,18 @@
 #include "hash_tables.h"
 
-
-
 /**
+ *  key_index - Creates a hash table
  *
- *  *  key_index - Creates a hash table
+ *  @key: Key to hashtable
  *
- *   *
+ *  @size: Size of the array
  *
- *    *  @key: Key to hashtable
- *
- *     *
- *
- *      *  @size: Size of the array
- *
- *       *
- *
- *        *  Return: A pointer to the newly created hash table
- *
- *         */
-
-
+ *  Return: A pointer to the newly created hash table
+ */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
-
 {
+unsigned long int djb2 = hash_djb2(key) % size;
 
-		unsigned long int djb2 = hash_djb2(key) % size;
-
-
-
-			return (djb2);
-
+return (djb2);
 }
